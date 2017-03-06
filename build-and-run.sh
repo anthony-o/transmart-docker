7f56e15e331e7987f249d189083f7fbf09b7595f
Anthony O <Anthony.Ogier-ext@sanofi.com>
2017-03-06 16:26:08 +0100

    Migrate the Rserve instance & RStudio to a different CentOS container (in order to correctly use Centrify PAM account management which needs an "rpm"-style OS)

--- build-and-run.sh
+++ build-and-run.sh
@@ -25,6 +25,28 @@ for PROXY_VAR in HTTPS_PROXY HTTP_PROXY https_proxy http_proxy ; do
     fi
 done
 
+# Build R server image (Sanofi specific: need a Rserve image based on CentOS in order for Centrify - auth mechanism - to be installed and work correctly)
+cd $BASE_SCRIPT_DIR/rserver
+PREVIOUS_TRANSMART_RSERVER_IMAGE_ID=$(docker images --quiet transmart_rserver || echo "")
+docker build --build-arg https_proxy=$https_proxy_IN_CONTAINER --build-arg http_proxy=$http_proxy_IN_CONTAINER -t transmart_rserver ./
+
+PREVIOUS_TRANSMART_RSERVER_BASE_IMAGE_ID=$(cat /var/local/transmart/built_transmart_rserver_base_image_id || echo "")
+CURRENT_TRANSMART_RSERVER_BASE_IMAGE_ID=$(docker images --quiet transmart_rserver)
+if [ "$PREVIOUS_TRANSMART_RSERVER_BASE_IMAGE_ID" != "$CURRENT_TRANSMART_RSERVER_BASE_IMAGE_ID" ] ; then
+    # The previous transmart_rserver docker image does not correspond to current build, let's continue the build and install Centrify tools - Sanofi specific (thanks to https://github.com/docker/docker/issues/14080#issuecomment-269460330 idea)
+    if [ ! -f /cdc/centrifydc-install.sh ] ; then
+        echo "Must mount /cdc first (this must point to Centrify installation tools). Call GAHS or Analytics Platforms and Services if you don't know what it is" >&2
+        exit 1
+    fi
+    docker rm transmart_rserver_build || echo "'transmart_rserver_build' container does not exist, that's normal."
+    docker run -it --name transmart_rserver_build -v /cdc:/cdc transmart_rserver /cdc/centrifydc-install.sh -r AMER
+    docker commit '--change=CMD ["/usr/local/bin/cmd.sh"]' transmart_rserver_build transmart_rserver
+    docker rm transmart_rserver_build
+    echo $CURRENT_TRANSMART_RSERVER_BASE_IMAGE_ID | dzdo tee /var/local/transmart/built_transmart_rserver_base_image_id
+else
+    docker tag $PREVIOUS_TRANSMART_RSERVER_IMAGE_ID transmart_rserver
+fi
+
 docker-compose -f $BASE_SCRIPT_DIR/transmart-$TARGET_ENV.yml build $*
 docker-compose -f $BASE_SCRIPT_DIR/transmart-$TARGET_ENV.yml rm -f $*
 docker-compose -f $BASE_SCRIPT_DIR/transmart-$TARGET_ENV.yml up -d $*
---
+++ rserver/cmd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+
+# Copy all files of host pam.d locally
+cp -ar /etc/pam.d.host/* /etc/pam.d/
+cp /etc/pam.d/login /etc/pam.d/rstudio # In order to correctly make RStudio to log users using local PAM configuration - with Kerberos
+
+# Add X11 support in R thanks to http://stackoverflow.com/a/1710952/535203 and https://gist.github.com/jterrace/2911875
+Xvfb :0 -ac -screen 0 1960x2000x24 &
+
+# Starting Rserve, inspired by https://github.com/tranSMART-Foundation/transmart-data/blob/2007abcf12b9d734b9b74348733e078e5ba014a2/R/rserve.conf.php#L25
+R CMD Rserve --quiet --vanilla
+# Start RStudio Server
+# rstudio-server start # doesn't work in this CentOS container, fails with message "Reloading systemd:  Failed to get D-Bus connection: Operation not permitted" which points to https://github.com/docker/docker/issues/7459
+#/etc/init.d/rstudio-server start
+
+# Tail the RStudio server logs thanks to https://support.rstudio.com/hc/en-us/articles/200554766-RStudio-Server-Application-Logs
+# tail -f /var/log/messages # Doesn't work: there is nothing at the 3 locations pointed by the article, they are at another locations http://stackoverflow.com/a/42511897/535203
+
+exec /usr/sbin/init # To correctly start D-Bus thanks to https://forums.docker.com/t/any-simple-and-safe-way-to-start-services-on-centos7-systemd/5695/8
